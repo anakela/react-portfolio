b4d5b04816b81d70bef6bb2f3bf8ad009af91b97
Angie <103882157+anakela@users.noreply.github.com>
2022-09-18 14:00:02 -1000

    Updated PortfolioContainer.js with navigation info.

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
+
+        if (currentPage === 'About') {
+            return <About />;
+        }
+
+        if (currentPage === 'Work') {
+            return <Work />;
+        }
+
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+
+        return <Resume />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
+};
