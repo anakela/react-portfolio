3158ed8fc19f3998987f7258b1206b7da0d34cdb
Angie <103882157+anakela@users.noreply.github.com>
2022-10-08 10:46:03 -1000

    Updates

---
+++ precache-manifest.e88c7cd0e8e95490ffc1f542a34c5242.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4b6b8eb5a47c803113e",
+    "url": "/react-portfolio/static/css/main.32df713e.chunk.css"
+  },
+  {
+    "revision": "e4b6b8eb5a47c803113e",
+    "url": "/react-portfolio/static/js/main.e4b6b8eb.chunk.js"
+  },
+  {
+    "revision": "ad39ee43e5f1e39ec27e",
+    "url": "/react-portfolio/static/js/1.ad39ee43.chunk.js"
+  },
+  {
+    "revision": "75cd2f2719bbe9ece27b",
+    "url": "/react-portfolio/static/js/2.75cd2f27.chunk.js"
+  },
+  {
+    "revision": "079c2b40f592520760c6",
+    "url": "/react-portfolio/static/js/runtime~main.079c2b40.js"
+  },
+  {
+    "revision": "a30f8c49aa500dcca83eea2ad396ca46",
+    "url": "/react-portfolio/static/media/angie-370px.a30f8c49.png"
+  },
+  {
+    "revision": "276db9a0583b69155ba443a194f2f060",
+    "url": "/react-portfolio/static/media/nanarwhal-1200px.276db9a0.jpg"
+  },
+  {
+    "revision": "ff7b65d49a436a27bd6dd0e8d51e1af6",
+    "url": "/react-portfolio/static/media/freakimono.ff7b65d4.jpg"
+  },
+  {
+    "revision": "38a93236efa2dc1ce47281e4923d62ca",
+    "url": "/react-portfolio/static/media/weather-dashboard.38a93236.png"
+  },
+  {
+    "revision": "e03534084af2eef37892e44548857eae",
+    "url": "/react-portfolio/static/media/nosql.e0353408.png"
+  },
+  {
+    "revision": "e54e407e6d764c35cf23200a54671eaa",
+    "url": "/react-portfolio/static/media/mvc-tech-blog.e54e407e.png"
+  },
+  {
+    "revision": "7697a309a9ddb3d23ba9c7cac1b23f9e",
+    "url": "/react-portfolio/index.html"
+  }
+];
