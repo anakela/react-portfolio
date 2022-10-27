7efe023a1518924cf4cf872fa649affe5a3cc06f
Angie <103882157+anakela@users.noreply.github.com>
2022-10-26 19:31:14 -1000

    Added project data to dynamically create cards on the Portfolio page.

---
+++ src/projectData/projectData.js
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+        title: "Bone Buddies by Team Turcrickeseal",
+        github: "https://github.com/Unicorn-Barf/Doggy_Dating",
+        deployedSite: "https://dev-bone-buddies.herokuapp.com/",
+        image: "../images/projects/bone-buddies-logo.png",
+        description: "An app for dog owners to find and interact with dog owners and dog playmates for their dogs.",
+    },
+    {
+        title: "Freakimono by Team NaNarwhal",
+        github: "https://github.com/Team-Narwhal/My_Pet",
+        deployedSite: "https://freakimono.herokuapp.com/",
+        image: "../images/projects/freakimono.jpg",
+        description: "A digital pet game where users can create and care for a digital pet of their choice. Pets can interact with one another via a battle system, thereby improving their health and wellbeing.",
+    },
+    {
+        title: "NaNarWord by Team NaNarwhal",
+        github: "https://github.com/NaNifer/NaNarWord-word-game",
+        deployedSite: "https://nanifer.github.io/NaNarWord-word-game/",
+        image: "../images/projects/nanarwhal-1200px.jpg",
+        description: "A word game we describe as Wordle's quirky cousin who is partnered with Merriam-Webster so it's smart and vivacious.",
+    },
+    {
+        title: "PWA Text Editor",
+        github: "https://github.com/anakela/pwa-text-editor",
+        deployedSite: "https://anakela-text-editor.herokuapp.com/",
+        image: "../images/projects/nosql.png",
+        description: "A text editor that utilizes PWA and service workers to work both on and offline.  Can be installed locally as an app as well.",
+    },
+    {
+        title: "MVC Tech Blog",
+        github: "https://github.com/anakela/mvc-tech-blog",
+        deployedSite: "https://anakela-mvc-tech-blog.herokuapp.com/",
+        image: "../images/projects/mvc-tech-blog.png",
+        description: "A blog that allows users to sign in, sign up, sign out, create blogs, delete blogs, add comments to blog entries, and delete the comments they've posted.",
+    },
+];
