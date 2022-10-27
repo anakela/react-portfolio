6a483b04b8a6ae17fa55f632bff8dadde30d3d4e
Angie <103882157+anakela@users.noreply.github.com>
2022-10-26 21:45:06 -1000

    Created a new component for Cards.js.

---
+++ src/components/Cards.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Card from '@mui/joy/Card';
+import { CardMedia, Grid } from '@mui/material';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+
+export default function ProjectCard({ title, github, deployedSite, image, description }) {
+    return (
+        <Grid container spacing={{ xs: 4, md: 3 }} style={{ justifyContent: "center" }}>
+            <Grid item md={6}>
+                <Card
+                    variant="outlined"
+                    className="card"
+                    sx={{ minWidth: 345, borderRadius: '10px', width: '100%' }}
+                    style={{ border: "5px solid var(--dark-purp)" }}
+                >
+                    <CardActionArea
+                        href={{ github }}
+                        target="_blank"
+                        rel='noopener noreferrer'
+                    >
+                        <CardMedia
+                            component="img"
+                            height="200"
+                            image={{ image }}
+                            alt={{ title }}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                <h3>{{ title }}</h3>
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {{ description }}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Grid>
+        </Grid>
+    )
+};
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,22 +1,37 @@
 import * as React from 'react';
-import Card from '@mui/joy/Card';
-import { CardMedia, Grid } from '@mui/material';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/joy/CardContent';
-import Typography from '@mui/joy/Typography';
-import NaNarWord from '../images/projects/nanarwhal-1200px.jpg';
-import Freakimono from '../images/projects/freakimono.jpg';
-import NoSQL from '../images/projects/nosql.png';
-import Blog from '../images/projects/mvc-tech-blog.png';
-import BoneBuddies from '../images/projects/bone-buddies-logo.png';
+// import Card from '@mui/joy/Card';
+// import { CardMedia, Grid } from '@mui/material';
+// import CardActionArea from '@mui/material/CardActionArea';
+// import CardContent from '@mui/joy/CardContent';
+// import Typography from '@mui/joy/Typography';
+// import NaNarWord from '../images/projects/nanarwhal-1200px.jpg';
+// import Freakimono from '../images/projects/freakimono.jpg';
+// import NoSQL from '../images/projects/nosql.png';
+// import Blog from '../images/projects/mvc-tech-blog.png';
+// import BoneBuddies from '../images/projects/bone-buddies-logo.png';
 import '../../styles/root.css';
+import ProjectCard from '../Cards';
+import { projects } from '../../projectData/projectData';
 
 export default function Work() {
     return (
         <div className='main-container'>
             <h2 id="work-header">My Work</h2>
-            <Grid container spacing={{ xs: 4, md: 3}} style={{ justifyContent: "center" }}>
-                <Grid item md={6}>
+            <div id="project-card-container">
+                {
+                    projects.map((project, index) => {
+                        <ProjectCard
+                            key={index}
+                            title={project.title}
+                            github={project.github}
+                            deployedSite={project.deployedSite}
+                            image={project.image}
+                            description={project.description}
+                        />
+                    })
+                }
+            </div>
+            {/* <Grid item md={6}>
                     <Card
                         variant="outlined"
                         className="card"
@@ -159,8 +174,7 @@ export default function Work() {
                             </CardContent>
                         </CardActionArea>
                     </Card>
-                </Grid>
-            </Grid>
+                </Grid> */}
         </div>
     );
 }
