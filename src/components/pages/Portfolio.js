59e5f95d7075f9952eba72a5cbc28e8b49a00496
Angie <103882157+anakela@users.noreply.github.com>
2022-10-30 12:12:59 -1000

    Cleaned up code on Portfolio.js.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,14 +1,5 @@
 import * as React from 'react';
-// import Card from '@mui/joy/Card';
 import { Grid } from '@mui/material';
-// import CardActionArea from '@mui/material/CardActionArea';
-// import CardContent from '@mui/joy/CardContent';
-// import Typography from '@mui/joy/Typography';
-// import NaNarWord from '../images/projects/nanarwhal-1200px.jpg';
-// import Freakimono from '../images/projects/freakimono.jpg';
-// import NoSQL from '../images/projects/nosql.png';
-// import Blog from '../images/projects/mvc-tech-blog.png';
-// import BoneBuddies from '../images/projects/bone-buddies-logo.png';
 import '../../styles/root.css';
 import ProjectCard from '../Cards';
 import { projects } from '../../projectData/projectData';
